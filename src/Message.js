28c9fe7f86de1cead72ca1e5fe4845e0b86b3cd8
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-12 01:06:00 +0500

    Class component

---
+++ src/Message.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+// this is Class Component ...
+
+class Message extends Component
+{
+    render()
+    {
+        return(
+            <h1>Hii, I am learning react js</h1>
+        )
+    }
+}
+
+export default Message;
