1e6d1e6122421519a83a8b36f53bc07d0068b0f7
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-13 10:57:20 +0500

    state in functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import './App.css';
+import { useState } from 'react';
 import User from './User';
 import Message from './Message'
 
 function App() {
+  const [data,setDate] = useState(0); 
+  function updateData(){
+    setDate(data + 1);
+    // alert("Congratulations");
+  }
+  console.log("_____________");
   return (
     <div className="App">
       <h1>Hello</h1>
       <User />
       <Message />
+      <h4>{data}</h4>
+      <button onClick={updateData}>Update data</button>
     </div>
   );
 }
