d94a470883c0abf20e615acfeb9a22c537df74e6
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-16 14:53:58 +0500

    installed boostrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import { Button, Alert } from "react-bootstrap";
 
 function App()
 {
 
-  const[count, setCount] = useState(0)
-
-  useEffect(()=>{
-    console.log("UseEffect called:");
-  })
-  
-
   return (
     <div className="App">
-      <h1>UseEffect in react</h1>
-      <h3>Count: {count}</h3>
-      <button onClick={()=>setCount(count+1)}>Update Count</button>
-
-
+    <h1>Installed bootstrap</h1>
+    <Alert variant="success">
+      <Alert.Heading>Hey, nice to see you</Alert.Heading>
+      <p>
+        Aww yeah, you successfully read this important alert message. This
+        example text is going to run a bit longer so that you can see how
+        spacing within an alert works with this kind of content.
+      </p>
+      <hr />
+      <p className="mb-0">
+        Whenever you need to, be sure to use margin utilities to keep things
+        nice and tidy.
+      </p>
+    </Alert>
+      <Button onClick={()=>{alert("Hello, brother")}}>Click Me</Button>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
     </div>
   )
 
