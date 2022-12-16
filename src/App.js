e7e7b7c1b26b35c9ffc24385b41fdf7a5f96262c
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-16 13:49:06 +0500

    done useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-import { useState } from "react";
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class App extends React.Component{
-  constructor()
-  {
-    super();
+function App()
+{
 
-    this.state = {
-      count: 0
-    }
+  const[count, setCount] = useState(0)
 
-    console.log("Contructor");
-  }
+  useEffect(()=>{
+    console.log("UseEffect called:");
+  })
+  
 
-  // shouldComponentUpdate
-  shouldComponentUpdate()
-  {
-    // Bydefault it remains false and prevents updates in a component
-    console.log("should Component Update:",this.state.count);
-    if (this.state.count >= 3 && this.state.count <=7) {
-      return true;
-    }
-    return false;
-  }
+  return (
+    <div className="App">
+      <h1>UseEffect in react</h1>
+      <h3>Count: {count}</h3>
+      <button onClick={()=>setCount(count+1)}>Update Count</button>
 
-  render()
-  {
-    console.log("Render called:");
 
-    return(
-      <div className="App">
-        <h1>Should Component Update</h1>
-        <h3>Count: {this.state.count}</h3>
-        <button onClick={()=>this.setState({count:this.state.count+1})}>Update Counter</button>
-      </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 export default App;
