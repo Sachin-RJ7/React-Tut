c30f9ab013bfbfbc9f6565af54afaa5602f7dd2c
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-17 10:56:35 +0500

    done useRef hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import { useRef } from "react";
 import "./App.css";
-import Child from "./Child";
 
 function App()
 {
 
+  let inputRef = useRef(null)
 
-  function parentAlert(name)
+  function handleInput()
   {
-    alert(name)
+    console.log("function Call");
+    inputRef.current.value = "1000"
+    inputRef.current.focus()
+    inputRef.current.style.display="none"
   }
 
-  return (
+  return(
     <div className="App">
-      <Child alert={parentAlert} />
+      <h1>Use Ref in react</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleInput}>Handle Input</button>
     </div>
   )
-
 }
 
 export default App;
