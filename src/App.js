120f732a748ed8f20bf230571835524becd693a2
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-17 12:45:03 +0500

    done Uncontrolled component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 
-import { useState } from "react";
+import { useRef } from "react";
 import "./App.css";
 
-// controlled component
 
-// A controlled component is a type of form element in React that is controlled by the component's state. In other words, the value of a controlled component is determined by the value of the component's state, rather than by the value of the DOM element's value attribute.
+// Uncontrolled component
+
+// An uncontrolled component is a form element in React that is not controlled by the component's state. In other words, the value of an uncontrolled component is determined by the DOM element's value attribute, rather than by the component's state.
 
 function App()
 {
-
-  const [val, setValue] = useState("Enter your name");
-
   
+  let inputRef1 = useRef(null)
+  let inputRef2 = useRef(null)
+  let inputRef3 = useRef(null)
+
+  function submitForm(e)
+  {
+    e.preventDefault();
+    console.log("input field 1",inputRef1.current.value)
+    console.log("input field 2",inputRef2.current.value)
+    console.log("input field 3",inputRef3.current.value)
+  }
+
   return(
     <div className="App">
-      <h1>Controlled Component</h1>
-      <input type="text" value={val} onChange={(e)=>setValue(e.target.value)} />
+
+      <h1>Uncontrolled Component</h1>
+
+      <form onSubmit={submitForm}>
+        <input type="text" ref={inputRef1 } /><br /> <br />
+        <input type="text" ref={inputRef2} /><br /> <br />
+        <input type="text" ref={inputRef3} /><br /> <br />
+        <button type="submit" >Submit</button>         
+      </form>
+
     </div>
   )
+
 }
 
 export default App;
