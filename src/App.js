c0ca096c06ca06c80afd075bc619d55d9fccfac6
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-19 21:56:35 +0500

    previous state

--- src/App.js
+++ src/App.js
@@ -1,59 +1,30 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 function App() {
-  const [users, setUser] = useState([]);
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  function getUsers() {
-    fetch("http://localhost:4000/todo").then((result) => {
-      result.json().then((resp) => {
-        // console.warn(resp)
-        setUser(resp);
-      });
-    });
-  }
-
-  console.warn(users);
-  function deleteUser(id) {
-    fetch(`http://localhost:4000/todo/${id}`, {
-      method: "DELETE",
-    }).then((result) => {
-      result.json().then((resp) => {
-        console.warn(resp);
-        getUsers();
-      });
-    });
-  }
+  const [count,setCount]=useState(0)
   
+  function updateCounter()
+  {
+    let item=Math.floor(Math.random()*10)
+    setCount((pre)=>{
+      if(pre<3)
+      {
+        alert("last value is very low")
+      }
+      return item
+    })
+    // for(let i=0;i<5;i++)
+    // {
+    //   setCount((pre)=>pre+1)
+    // }
+  }
+
   return (
     <div className="App">
-      <h1>Delete User With API </h1>
-      <table border="1" style={{ float: "left" }}>
-        <tbody>
-          <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Mobile</td>
-            <td>Operations</td>
-          </tr>
-          {users.map((item, i) => (
-            <tr key={i}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>{item.mobile}</td>
-              <td>
-                <button onClick={() => deleteUser(item.id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+       <h1>{count}</h1>
+      <button  onClick={updateCounter}>Click Me to Update counter</button>
     </div>
-  );
+  )
 }
 
 export default App;
